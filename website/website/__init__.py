b1ea65a04fe00cd7c217ce8ddafc119d85779935
John Ray Bergado <johnraybergado@gmail.com>
2015-04-12 13:23:03 +0200

    data pages

--- website/website/__init__.py
+++ website/website/__init__.py
@@ -13,6 +13,34 @@ def world():
 def netherlands():
     return render_template('netherlands.html')
 
+@app.route('/data')
+def netherlands():
+    return render_template('data.html')
+
+@app.route('/cereals')
+def netherlands():
+    return render_template('cereals.html')
+
+@app.route('/fruits')
+def netherlands():
+    return render_template('fruits.html')
+
+@app.route('/meat')
+def netherlands():
+    return render_template('meat.html')
+
+@app.route('/oilseeds')
+def netherlands():
+    return render_template('oilseeds.html')
+
+@app.route('/othercashcrops')
+def netherlands():
+    return render_template('othercashcrops.html')
+
+@app.route('/roots')
+def netherlands():
+    return render_template('roots.html')
+
 @app.route('/detailed/')
 @app.route('/detailed/<country>')
 def detailed(country=None):
