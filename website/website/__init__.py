994591b28898bdf8ed633da955040df37f069862
John Ray Bergado <johnraybergado@gmail.com>
2015-04-12 08:38:55 +0200

    fix url sample 1 integration

--- website/website/__init__.py
+++ website/website/__init__.py
@@ -9,13 +9,11 @@ def index():
 def world():
     return render_template('world.html')
 
-@app.route('/world/netherlands')
-def netherlands():
-	return render_template('netherlands.html')
-
 @app.route('/detailed/')
 @app.route('/detailed/<country>')
 def detailed(country=None):
+	if country == "Netherlands":
+		return render_template('netherlands.html', country=country)
 	return render_template('detailed.html', country=country)
 
 if __name__ == '__main__':
